3e79945ca7b6eebea04411fa9f0ad9039ca0fb2c
Nizami <nizami@about-me.app>
2022-12-19 11:18:38 +0500

    Initial Commit

---
+++ NatlexWeather/NatlexWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatlexWeatherApp.swift
+//  NatlexWeather
+//
+//  Created by Nizami Tagiyev on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NatlexWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
